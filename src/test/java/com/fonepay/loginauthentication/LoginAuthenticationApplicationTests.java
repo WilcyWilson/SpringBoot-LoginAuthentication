4d695c40695131e58c844c80fc16395f40aaa039
WilcyWilson <wilsonshresthaapps@gmail.com>
2021-12-27 16:57:23 +0545

    Initial commit

---
+++ src/test/java/com/fonepay/loginauthentication/LoginAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonepay.loginauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
